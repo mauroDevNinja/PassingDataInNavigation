226e6216d6f88e07410c10d39bc0680d7fcb7c86
Mauricio Andrés Contreras Contreras <m_contreras@outlook.cl>
2015-08-31 05:19:20 -0300

    Commit Finished Demo
    Contiene contenido final de la demo para pasar datos y objetos entre
    pantallas con método Navigate en Windows 10.

---
+++ MyFirstApp/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace PassingDataInNavigation
+{
+    public sealed partial class MainPage : Page
+    {
+        Datos myData = new Datos();
+        public MainPage()
+        {
+            InitializeComponent();           
+        }
+
+        private void btn_Avanzar_Click(object sender, RoutedEventArgs e)
+        {
+            myData.myName = tb_Name.Text;
+            myData.myLastName = tb_LastName.Text;
+            Frame.Navigate(typeof(Page2), myData);
+        }
+    }
+}
---
+++ MyFirstApp/Page2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace PassingDataInNavigation
+{ 
+    public sealed partial class Page2 : Page
+    {
+        public Page2()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Datos passedData = e.Parameter as Datos;
+            string myName = passedData.myName;
+            string myLastName = passedData.myLastName;
+
+            if (!string.IsNullOrWhiteSpace(myName)) 
+            {
+                tb_passedName.Text = "Bien hecho! " + myName;
+                tb_passedLastName.Text = myLastName;
+            }
+            else
+            {
+                tb_passedName.Text = "Algo malo pasó :( inténtalo de nuevo!";
+                tb_passedLastName.Text = "No ingresaste apellido :(";
+            }
+        }
+
+        private void btn_Volver_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
